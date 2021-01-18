1e77eee0cbfe8b52ac8169d7d25dca7c71a45aea
leiqlxk <472990991@qq.com>
2021-01-18 22:56:10 +0800

    插槽及模块化

--- 12-前端模块化/02-ES6的模块化实现/mmm.js
+++ 12-前端模块化/02-ES6的模块化实现/mmm.js
@@ -2,6 +2,11 @@ import {flag, sum} from "./aaa.js";
 
 //默认导入的就是export default的那个
 import addr from "./aaa.js";
+
+//全部导入
+import * as aaa from "./aaa.js";
+
+console.log(aaa.flag);
 if (flag) {
   console.log('小明是个天才');
 }
