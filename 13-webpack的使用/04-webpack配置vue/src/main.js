ed064c331b3b1302458a8ce2ef75d152bc59693b
leiqlxk <472990991@qq.com>
2021-01-26 22:10:02 +0800

    vue配置

---
+++ 13-webpack的使用/04-webpack配置vue/src/main.js
@@ -0,0 +1,47 @@
+
+//Commonjs导入
+const {add, mul} = require('./js/mathUtils');
+
+console.log(add(20, 56));
+
+console.log(mul(25, 6));
+
+//ES6导入
+import {name, age, height} from "./js/info";
+
+console.log(name);
+
+console.log(age);
+
+console.log(height);
+
+//依赖css文件，有依赖才会跟我们写的东西产生引用去打包进来，因为css不想引用js中的属性或者方法等需要使用，所以我们都不需要新建变量来接收，引用进来就行
+//但是正常webpack不支持打包css，我们需要通过通过不同的loader来打包css、scss、less等内容，不同的loader在官网中都有https://webpack.js.org/，翻译后的网站为https://www.webpackjs.com/
+//loader的使用步骤为：
+//1.通过npm安装需要使用的loader
+//2.在webpack.config.js中的module关键字下进行配置
+
+require('./css/normal.css');
+
+require('./css/special.less');
+
+document.writeln('<h2>你好啊,webpack</h2>')
+
+//使用Vue进行开发
+//在npm install vue后在node_modules中就用vue的引用了，并且其通过export default Vue导出
+import Vue from 'vue';
+
+new Vue({
+  el: '#app',
+  //vue在编译的时候会把template中的内容去替换el挂载的div
+  template: `
+    <div>
+      <h2>{{message}}</h2>
+    </div>
+  `,
+  data() {
+    return {
+      message: '哈哈哈哈'
+    }
+  }
+});
---
+++ 13-webpack的使用/04-webpack配置vue/webpack.config.js
@@ -0,0 +1,76 @@
+//首先从node中导入path包，以便后面可以动态获取绝对路径
+//一旦要使用node的东西，最好立马执行npm init初始化项目会生成package.json,里面说明了引用了哪些包也就是node管理包的核心文件
+//然后执行npm install安装package中的依赖包，目前只是使用node中的path没有依赖别的相关包所以install也没有效果
+const path = require('path');
+
+//webpack本身使用的是CommonJS的规范导出配置
+module.exports = {
+  //入口
+  entry: './src/main.js',
+  //出口是对象，包含路径和文件名且path必须为绝对路径
+
+  output: {
+    //path.resolve()拼接字符串的函数, __dirname是node中的全局变量获取当前项目上下文的路径
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    //在index.html文件没有打包进dist中时引用图片时应该引用打包后的文件，即dist中的文件
+    // 项目中所有用到url的时候默认前缀，例如图片文件等
+    publicPath: 'dist/'
+  },
+  //配置webpack所用到的扩展模块，css-loader只负责加载不会将样式加到dom里，还需要配置style-loader
+  module: {
+    rules: [
+      {
+        //正则表达式
+        test: /\.css$/,
+        //使用多个loader时从右向左读
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "less-loader" // compiles Less to CSS
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              //高版本默认情况下，文件加载器生成使用ES modules语法的JS模块，此处我们直接使用了url而没有导入，可以使用CommonJS
+              esModule: false,
+              //当图片大小下于该值时，会自动转换成base64显示，大于该值时会打包文件,则需要使用到file-loader，file-loader不需要配置，直接安装即可
+              //一般设置8KB即
+              limit: 8192,
+              //设置打包后的文件夹及文件名规范
+              name: 'img/[name].[hash:8].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        //把es6语法转换成es5
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      }
+    ]
+  },
+  //配置runtime-compiler
+  resolve: {
+    alias: {
+      // 配置我们import的vue具体指向的文件，vue.esm.js中包含了compiler，默认指向的是vue.runtime.js
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+}
