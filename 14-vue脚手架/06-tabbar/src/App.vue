012f0312cff59cbf579eaef0794dbf89f372e4a9
leiqlxk <472990991@qq.com>
2021-02-24 23:16:28 +0800

    tabbar封装

---
+++ 14-vue脚手架/06-tabbar/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+import MainTabBar from 'components/MainTabBar'
+
+export default {
+  name: 'App',
+  components: {
+    MainTabBar
+  }
+}
+</script>
+
+<style>
+  /*相当于js中的require（）*/
+  @import "assets/css/base.css";
+
+</style>
