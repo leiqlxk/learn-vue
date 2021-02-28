368dbd564cdccb7b2b3685dc9e119e5bb0c88fc6
leiqlxk <472990991@qq.com>
2021-02-28 23:39:01 +0800

    vuex

---
+++ 16-Vuex/01-learnvuex/src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// 1.安装插件
+Vue.use(Vuex)
+
+// 2.创建对象
+const store = new Vuex.Store({
+    state: {
+      count: 5
+    },
+    mutations: {},
+    actions: {},
+    getters: {},
+    modules: {}
+  }
+)
+
+// 3.导出对象
+export default store
