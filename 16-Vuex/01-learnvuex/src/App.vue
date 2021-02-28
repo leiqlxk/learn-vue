2e16706ab7f245f6e225449c6baf9838797a0e23
leiqlxk <472990991@qq.com>
2021-02-28 23:39:45 +0800

    vuex

--- 16-Vuex/01-learnvuex/src/App.vue
+++ 16-Vuex/01-learnvuex/src/App.vue
@@ -1,28 +1,35 @@
 <template>
+  <!--  view-->
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h2>{{message}}</h2>
+    <h2>{{$store.state.count}}</h2>
+    <!--    行为-->
+<!--    直接操作state中的变量是可以的，但是官方不建议-->
+    <button @click="$store.state.count++">+</button>
+    <button @click="$store.state.count--">-</button>
+    <!--父子组件使用props传值-->
+    <!--   当其不为父子组件时，传值就需要传递很多次，容易造成混乱，则可以使用vuex来管理，共享状态数据-->
+    <hello-vuex />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloVuex from "./components/HelloVuex";
 
 export default {
   name: 'App',
+  //状态
+  data() {
+    return {
+      message: 'Hello Vuex',
+      // count: 0
+    }
+  },
   components: {
-    HelloWorld
+    HelloVuex
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
