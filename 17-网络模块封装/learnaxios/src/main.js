abdf00e8f2361b2f6254ec211983ffcfca7ff639
leiqlxk <472990991@qq.com>
2021-03-03 21:08:10 +0800

    axios封装

--- 17-网络模块封装/learnaxios/src/main.js
+++ 17-网络模块封装/learnaxios/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import axios from "axios";
 
 Vue.config.productionTip = false
 
@@ -8,3 +9,112 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+//简单的get请求，method不写也可以，默认的就是get请求，返回的是一个Promise
+axios({
+  //httpbin.org可以进行网络测试
+  url: 'http://httpbin.org',
+  method: 'get'
+}).then(res => {
+  console.log(res);
+})
+
+//axios全局配置
+axios.defaults.baseURL = 'http://httpbin.org'
+axios.defaults.timeout = 5000
+
+axios({
+  //httpbin.org可以进行网络测试
+  url: '/get',
+  method: 'get',
+  //专门针对get请求的参数拼接，或者自己通过?拼接到地址后面也可以
+  params: {
+    type: 'pop',
+    page: 1
+  }
+}).then(res => {
+  console.log(res);
+})
+
+//两个请求都要完成才能往下走,类似于Promise.all,返回的是一个数组
+axios.all([axios({
+  url: '/get'
+}), axios({
+    url: 'get'
+  })]).then(results => {
+  console.log(results);
+})
+
+//返回的数组可以是用axios.spread来解开
+axios.all([axios({
+  url: 'http://httpbin.org'
+}), axios({
+  url: 'http://httpbin.org'
+})]).then(axios.spread((res1, res2) =>
+{
+  console.log(res1);
+  console.log(res2);
+}))
+
+//正常我们不会直接使用全局的axios，而是取创建一个实例，这样我们就可以创建多个实例，根据接口去选择使用哪个实例
+const instance = axios.create({
+  baseURL: 'http://****.****.***:****',
+  timeout: 5000
+})
+
+instance({
+  url: '/***/***'
+}).then(res => {
+  console.log(res);
+})
+
+
+//1. 通过传入回调函数使用封装后的reques模块
+import {request} from './network/request'
+
+request({
+  url: '/home'
+}, res => {
+  console.log(res);
+}, err => {
+  console.log(err);
+})
+
+//2. config和回调函数封装在一个对象中的reques模块
+import {request1} from './network/request'
+
+request1({
+  baseConfig: {
+    url: '/home'
+  },
+  success(res) {
+
+  },
+  failure(err) {
+
+  }
+})
+
+//3.使用Promise方式
+import {request2} from './network/request'
+
+request2({
+  url: '/home'
+}).then(res => {
+  console.log(res);
+}).catch(err => {
+  console.log(err);
+})
+
+//4.最终方式
+import {request3} from './network/request'
+
+request3({
+  url: '/home'
+}).then(res => {
+  console.log(res);
+}).catch(err => {
+  console.log(err);
+})
+
+
---
+++ 17-网络模块封装/learnaxios/src/network/request.js
@@ -0,0 +1,118 @@
+//封装网络请求，之所以不用export default是可能扩展为多个实例
+import axios from "axios";
+
+//1.传入回调函数封装
+export function request(config, success, failure) {
+  //1.创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://httpbin.org',
+    timeout: 5000
+  })
+
+  //发送真正的网络请求
+  instance(config)
+    .then(res => {
+      console.log(res)
+      success(res)
+    })
+    .catch(err => {
+      console.log(err)
+      failure(err)
+    })
+}
+
+//2.将config和回调合并在一起进行封装
+export function request1(config) {
+  //1.创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://httpbin.org',
+    timeout: 5000
+  })
+
+  //发送真正的网络请求
+  instance(config.baseConfig)
+    .then(res => {
+      console.log(res)
+      config.success(res)
+    })
+    .catch(err => {
+      console.log(err)
+      config.failure(err)
+    })
+}
+
+//3.Promise方式
+export function request2(config) {
+  return new Promise(((resolve, reject) => {
+    //1.创建axios的实例
+    const instance = axios.create({
+      baseURL: 'http://httpbin.org',
+      timeout: 5000
+    })
+
+    //发送真正的网络请求
+    instance(config.baseConfig)
+      .then(res => {
+        resolve(res)
+      })
+      .catch(err => {
+        reject(err)
+      })
+  }))
+}
+
+//4.最终方案，其实axios本来就是使用的promise方式，直接返回就可以
+export function request3(config) {
+  //1.创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://httpbin.org',
+    timeout: 5000
+  })
+
+  //发送真正的网络请求
+  return instance(config.baseConfig)
+}
+
+
+//6.axios拦截器
+export function request4(config) {
+  //1.创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://httpbin.org',
+    timeout: 5000
+  })
+
+  //发送真正的网络请求
+  return instance(config.baseConfig)
+}
+
+//拦截请求
+request4.interceptors.request.use(
+  //成功处理
+  config => {
+    console.log(coonfig);
+    //可以在此进行处理的东西
+    //1.比如config中的一些信息不符合服务器的要求
+    //2.比如每次发送网络请求时，都希望在界面中显示一个请求的图标
+    //3.某些网络请求，必须携带一些特殊的信息（如token），必须进行校验
+
+    //最后要把config返回出去，不然最终请求的时候就没有config了
+    return config
+  },
+  //失败处理
+  err => {
+    console.log(err);
+  }
+)
+
+//拦截响应
+request4.interceptors.response.use(
+  res => {
+    console.log(res);
+    //一般只需要data吧
+    return res.data
+  },
+  err => {
+    console.log(err);
+  }
+)
